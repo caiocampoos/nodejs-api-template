36aab10ae1d78bda93b31f402bd7385b08a75c12
caiocampos <caiocampoosbr@gmail.com>
2023-02-16 00:31:43 -0300

    add: orm and config

--- src/plugins/config.ts
+++ src/plugins/config.ts
@@ -2,7 +2,6 @@ import "dotenv/config";
 import fp from "fastify-plugin";
 import { FastifyPluginAsync } from "fastify";
 import { Static, Type } from "@sinclair/typebox";
-import Ajv from "ajv";
 
 export enum NodeEnv {
   development = "development",
@@ -16,28 +15,15 @@ const ConfigSchema = Type.Strict(
     LOG_LEVEL: Type.String(),
     API_HOST: Type.String(),
     API_PORT: Type.String(),
+    API_NAME: Type.String(),
+    JWT_SECRET: Type.String()
   })
 );
-
-const ajv = new Ajv({
-  allErrors: true,
-  removeAdditional: true,
-  useDefaults: true,
-  coerceTypes: true,
-  allowUnionTypes: true,
-});
 
 export type Config = Static<typeof ConfigSchema>;
 
 const configPlugin: FastifyPluginAsync = async (server) => {
-  const validate = ajv.compile(ConfigSchema);
-  const valid = validate(process.env);
-  if (!valid) {
-    throw new Error(
-      ".env file validation failed - " +
-        JSON.stringify(validate.errors, null, 2)
-    );
-  }
+
   server.decorate("config", process.env);
 };
 
